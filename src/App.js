118eaa6f880adc83b5226e9561a8d79e721f11b0
randaman23 <randaman234@gmail.com>
2018-12-04 10:56:53 -0700

    making cards with higher order

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import topImg from "./top.png";
+import middleImg from "./middle.png";
+import bottomImg from "./bottom.png";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Card2
+          renderImg={() => <img src={middleImg} alt="" />}
+          renderBottom={() => <img src={bottomImg} alt=""/>}
+        />
+        <Card>
+          <div>
+            <img src={topImg} alt="" />
+            <img src={middleImg} alt="" />
+            <img src={bottomImg} alt="" />
+          </div>
+        </Card>
       </div>
     );
   }
 }
 
 export default App;
+
+class Card extends Component {
+  render() {
+    return <div className="card">{this.props.children}</div>;
+  }
+}
+
+class Card2 extends Component {
+  render() {
+    return (
+      <div className="card">
+        <h5>
+          {this.props.name} - {this.props.hp}
+        </h5>
+        {this.props.renderImg()}
+        {this.props.renderBottom()}
+      </div>
+    );
+  }
+}
