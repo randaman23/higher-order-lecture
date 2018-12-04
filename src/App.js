be3c92e5325277892bf265db3be20c8b59e62988
randaman23 <randaman234@gmail.com>
2018-12-04 11:18:19 -0700

    added list of tacos

--- src/App.js
+++ src/App.js
@@ -2,16 +2,44 @@ import React, { Component } from "react";
 import topImg from "./top.png";
 import middleImg from "./middle.png";
 import bottomImg from "./bottom.png";
-
 import "./App.css";
+
+const tacos = [
+  {
+    meat: "chicken",
+    cheese: "swiss",
+    veggies: "mushrooms, bacon",
+    name: "cordon blue"
+  },
+  {
+    meat: "steak",
+    cheese: "provolone",
+    veggies: "pineapple",
+    name: "steak boi"
+  },
+  {
+    meat: "elk",
+    cheese: "cheddar",
+    veggies: "olives",
+    name: "elkinator"
+  }
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <List
+          data={tacos}
+          renderItem={taco => (
+            <li>
+              {taco.name} : {taco.meat} + {taco.cheese} + {taco.veggies}
+            </li>
+          )}
+        />
         <Card2
           renderImg={() => <img src={middleImg} alt="" />}
-          renderBottom={() => <img src={bottomImg} alt=""/>}
+          renderBottom={() => <img src={bottomImg} alt="" />}
         />
         <Card>
           <div>
@@ -42,6 +70,19 @@ class Card2 extends Component {
         </h5>
         {this.props.renderImg()}
         {this.props.renderBottom()}
+      </div>
+    );
+  }
+}
+
+class List extends Component {
+  render() {
+    const { data } = this.props;
+    return (
+      <div>
+        {data.map(x => {
+          return this.props.renderItem(x);
+        })}
       </div>
     );
   }
